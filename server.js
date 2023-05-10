d5c95e1a0fd2838de835a21d50ca95ec862afab7
Blake H <blake.hollingsworth80@gmail.com>
2023-05-10 13:56:23 -0600

    added the api/notes get-request to display notes to the html

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 // Dependencies
 const express = require('express');
+const path = require('path')
+
+// retrieve db notes
+const notes = require('./db/db.json')
 
 // start the express app
 const app = express();
 const PORT = 3001;
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.get('/', (req, res) => { res.sendFile('index.html') })
+// get request for the basic homepage
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
 
+// get request for the notes page
+app.get('/notes', (req, res) => { res.sendFile(path.join(__dirname, 'public/notes.html')) })
+
+// retrieves api notes from db
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
+})
 
 app.listen(PORT, () => {
     console.log(`App is listening at http://localhost:${PORT}`)
