5d44755e0ab883ab027da476cd92d4c75f919e09
Blake H <blake.hollingsworth80@gmail.com>
2023-05-22 12:07:31 -0600

    fixed POST and GET request for api/notes - tutor help

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 // Dependencies
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs').promises;
 
 // retrieve db notes
 const notes = require('./db/db.json');
-const { log } = require('console');
 
 // start the express app
 const app = express();
@@ -25,33 +24,40 @@ app.get('/notes', (req, res) => { res.sendFile(path.join(__dirname, 'public/note
 
 // retrieves api notes from db
 app.get('/api/notes', (req, res) => {
-    res.json(notes)
+    fs.readFile('./db/db.json').then((notes) => {
+        console.log(JSON.parse(notes));
+        res.json(JSON.parse(notes));
+    })
 })
 
-app.post('/api/notes', (req, res) => {
-    // define variable for the scope
+// function to read file
+function readAndReturn(newNote) {
     var dbArray;
-    console.log(req.body)
-    fs.readFile('./db/db.json', (error, fileData) => {
+
+    fs.readFile('./db/db.json').then(fileData => {
         // turn the read file into a json arrray
         dbArray = JSON.parse(fileData);
-        console.log(dbArray)
         // push the request object into the array
-        dbArray.push(req.body);
+        dbArray.push(newNote);
         // turn the whole array back into a string
-        console.log(dbArray);
         dbArray = JSON.stringify(dbArray);
-    })
-    console.log(dbArray)
+        console.log(dbArray, 'array');
 
-    fs.writeFile('./db/db.json', dbArray, (err) => {
-        err ? console.log(err) : console.log('Data written to db.json')
-    })
+        fs.writeFile('./db/db.json', dbArray, (err) => {
+            err ? console.log(err) : console.log('Data written to db.json')
+        });
 
-    // fs.appendFile('./db/db.json', data, (err) => {
-    //     err ? console.log(err) : console.log('Data written to db.json')
-    // })
-})
+    });
+    // (error, fileData) => {
+    // }
+
+}
+
+app.post('/api/notes', async (req, res) => {
+    await readAndReturn(req.body)
+    res.end()
+
+});
 
 app.listen(PORT, () => {
     console.log(`App is listening at http://localhost:${PORT}`)
